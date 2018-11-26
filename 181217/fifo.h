b083ba89a3aebf458f34fec3dd207cf209f5ed81
Stefano <tommasinistefano@gmail.com>
2018-11-26 18:30:06 +0100

    test

--- 181217/fifo.h
+++ 181217/fifo.h
@@ -5,6 +5,98 @@
 #include <cassert>
 
 template <typename T>
+class fifo {
+private:
+  struct nodo {
+    T value;
+    nodo *next;
+
+    nodo(T val) : value(val), next(0) {}
+  };
+public:
+  nodo *_inizio;
+  nodo *_fine;
+
+  //Costruttore default
+  fifo() {
+    #ifndef NDEBUG
+    std::cout << "fifo()" << std::endl;
+    #endif
+    _inizio = 0;
+    _fine = 0;
+  }
+
+  //distruttore
+  ~fifo() {
+    #ifndef NDEBUG
+    std::cout << "~fifo()" << std::endl;
+    #endif
+    unsigned int n = conta_ele();
+    for(unsigned int i = 0; i < n; i++)
+      rimuovi();
+  }
+
+  fifo(const fifo &other) {
+    _inizio = other._inizio;
+    _fine = other._fine;
+  }
+
+  fifo &operator=(const fifo &other) {
+    fifo tmp = fifo(other);
+    while(other._inizio != 0){
+      accoda(tmp.rimuovi());
+    }
+    return *this;
+  }
+
+  //accoda elemento
+  void accoda(T value) {
+    nodo *tmp = new nodo(value);
+    if (_inizio != 0)
+      _fine -> next = tmp;
+    else
+      _inizio = tmp;
+    _fine = tmp;
+  }
+
+  T rimuovi(void) {
+    assert(_inizio != 0);
+    nodo *tmp = _inizio;
+    T ris = _inizio -> value;
+    _inizio = _inizio -> next;
+    delete tmp;
+    return ris;
+  }
+
+  unsigned int conta_ele(void) const {
+    unsigned int cont = 0;
+    if (_inizio == 0)
+      return cont;
+    nodo *tmp = _inizio;
+    while(tmp != _fine) {
+      cont++;
+      tmp = tmp -> next;
+    }
+    return cont + 1; //almeno un ele
+  }
+
+
+};
+
+//operatore di stampa
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const fifo<T> &f) {
+  fifo<T> tmp;
+  tmp = f;
+  os << " [ ";
+  while(tmp._inizio != 0)
+    os << tmp.rimuovi() << " ";
+  os << "] ";
+  return os;
+}
+
+/*
+template <typename T>
 class fifo{
 private:
   unsigned int _occ; //n. di valori dentro la coda
@@ -83,7 +175,7 @@ public:
     return _occ;
   }
 
-  void add(T value) {
+  void push(T value) {
     #ifndef NDEBUG
     std::cout << "add(v)" << std::endl;
     #endif
@@ -92,7 +184,7 @@ public:
     _occ++;
   }
 
-  void remove(void) {
+  void pop(void) {
     #ifndef NDEBUG
     std::cout << "remove" << std::endl;
     #endif
@@ -124,5 +216,6 @@ std::ostream &operator<<(std::ostream &os, const fifo<T> &f) {
   os << "] ";
   return os;
 }
+*/
 
 #endif
--- 181217/main.cpp
+++ 181217/main.cpp
@@ -5,10 +5,19 @@ int main(int argc, char const *argv[]) {
 
   std::cout << "Progetto 17/12/2018 - Coda F.I.F.O. - Tommasini Stefano 810929" << std::endl;
 
-  fifo<int> fi(7);
-  fi.add(8);
-  fi.add(1);
-  fi.add(34);
+  fifo<int> fi,fi2;
+
+  fi.accoda(1);
+  fi.accoda(2);
+  fi.accoda(3);
+  fi.accoda(4);
+  fi.accoda(5);
+  std::cout << "Numero di Elementi : " << fi.conta_ele() << std::endl;
+  fi2 = fi;
+  std::cout << fi2 << std::endl;
+  fi.rimuovi();
+  fi.rimuovi();
+  fi.rimuovi();
   std::cout << fi << std::endl;
   return 0;
 }
