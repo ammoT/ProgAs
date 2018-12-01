a729dd8e73d20847545a8a106d51372b0e9f6a55
Stefano <tommasinistefano@gmail.com>
2018-12-01 19:00:11 +0100

    ite

--- 181217/fifo.h
+++ 181217/fifo.h
@@ -1,99 +1,100 @@
-#ifndef FIFO_FIFO_H
-#define FIFO_FIFO_H
+#ifndef FIFO_H
+#define FIFO_H
 
 #include <iostream>
 #include <cassert>
 
 template <typename T>
 class fifo {
-
-    struct nodo {
-        T value;
-        nodo *next;
-        explicit nodo(const T val) : value(val), next(0) {}
-    };
+  struct nodo {
+    T value;
+    nodo *next;
+    explicit nodo(const T val) : value(val), next(0) {}
+  };
 private:
-    nodo * _inizio;
-    nodo * _fine;
+  nodo * _inizio;
+  nodo * _fine;
 
 public:
-    fifo() : _inizio(0), _fine(0) {}
-    ~fifo() {
-        while(_inizio != 0) {
-            rimuovi();
-        }
-        _inizio = 0;
-        _fine = 0;
+  fifo() : _inizio(0), _fine(0) {}
+  ~fifo() {
+    while(_inizio != 0)
+      rimuovi();
+    _inizio = 0;
+    _fine = 0;
+  }
+
+  fifo(const fifo &other) : _inizio(0), _fine(0) {
+    nodo *tmp = other._inizio;
+
+    while (tmp != 0) {
+      accoda(tmp -> value);
+      tmp = tmp -> next;
     }
+  }
 
-    fifo(const fifo &other){
-      nodo *tmp = other._inizio;
-      while(_inizio != 0) {
-        rimuovi();
-      }
+  template <typename IT>
+  fifo(IT inizio, IT fine) : _inizio(0), _fine(0) {
+    for (;inizio != fine; inizio++)
+    accoda(*inizio);
+  }
 
-      while (tmp != 0){
-        accoda(tmp -> value);
+  fifo &operator=(const fifo &other) {
+    nodo *tmp = other._inizio;
+    while(_inizio != 0) {
+      rimuovi();
+    }
+    while (tmp != 0){
+      accoda(tmp -> value);
+      tmp = tmp -> next;
+    }
+    return *this;
+  }
+
+  void accoda(const T &value) {
+    nodo *tmp = new nodo(value);
+    if (_inizio != 0)
+      _fine -> next = tmp;
+    else
+      _inizio = tmp;
+      _fine = tmp;
+  }
+
+  T rimuovi(void) {
+    assert( _inizio != 0);
+    T val = _inizio -> value;
+    nodo *tmp = _inizio;
+    _inizio = _inizio -> next;
+    delete tmp;
+    return val;
+  }
+
+  unsigned int size(void) const {
+    nodo *tmp = _inizio;
+    unsigned int cont = 0;
+    if (_inizio != 0) {
+      while (tmp != 0) {
         tmp = tmp -> next;
+        cont++;
       }
     }
-
-    fifo &operator=(const fifo &other) {
-      nodo *tmp = other._inizio;
-      while(_inizio != 0) {
-        rimuovi();
-      }
-      while (tmp != 0){
-        accoda(tmp -> value);
-        tmp = tmp -> next;
-      }
-      return *this;
-    }
-
-    void accoda(const T &value) {
-        nodo *tmp = new nodo(value);
-        if (_inizio != 0)
-            _fine -> next = tmp;
-        else
-            _inizio = tmp;
-        _fine = tmp;
-    }
-
-    T rimuovi(void) {
-        assert( _inizio != 0);
-        T val = _inizio -> value;
-        nodo *tmp = _inizio;
-        _inizio = _inizio -> next;
-        delete tmp;
-        return val;
-    }
-
-    unsigned int size(void) const {
-        nodo *tmp = _inizio;
-        unsigned int cont = 0;
-        if (_inizio != 0){
-          while (tmp != 0) {
-            tmp = tmp -> next;
-            cont++;
-        }
-      }
-        return cont;
-    }
+    return cont;
+  }
 };
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os,const fifo<T> &f) {
-
-    fifo<T> tmp;
-    tmp = f;
-    unsigned int sz = tmp.size();
-    std::cout << "SIZE : " << sz << std::endl;
-    std::cout << " [ ";
-    for (unsigned int i = 0; i < sz; ++i) {
-      std::cout << " [ "<<tmp.rimuovi() << " ] ";
-    }
-    os << "]";
-    return os;
+  fifo<T> tmp = f;
+  unsigned int sz = tmp.size();
+  os << "SIZE : " << sz << std::endl;
+  os << " [ ";
+  for (unsigned int i = 0; i < sz; ++i) {
+    if (i != 0)
+      os << ", ";
+    os << tmp.rimuovi();
+  }
+  os << " ] ";
+  return os;
 }
 
-#endif //FIFO_FIFO_H
+#endif //FIFO_H
--- 181217/main.cpp
+++ 181217/main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include "fifo.h"
+
+#include <vector>
+
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+  std::cout << std::endl;
   std::cout << "Progetto 17/12/2018 - Coda F.I.F.O. - Tommasini Stefano 810929" << std::endl;
-
+  std::cout << endl;
   fifo<int> fi;
-    fifo<std::string> fs;
-    fi.accoda(1);
-    fi.accoda(2);
-    fi.accoda(3);
-    fi.accoda(4);
-    fi.accoda(4);
+  fifo<std::string> fs;
+  fi.accoda(1);
+  fi.accoda(2);
+  fi.accoda(3);
+  fi.accoda(4);
+  fi.accoda(4);
 
-    fifo<int> copia;
-    //fifo<int> a = fi;
-    copia = fi;
+  fifo<int> copia;
+  fifo<int> a = fi;
+  copia = fi;
 
-    copia.rimuovi();
+  copia.rimuovi();
 
-    fs.accoda("a");
-    fs.accoda("B");
-    fs.accoda("C");
-    fs.accoda("CIAO");
+  fs.accoda("a");
+  fs.accoda("B");
+  fs.accoda("C");
+  fs.accoda("CIAO");
 
-    std::cout << fi << std::endl << std::endl;
-    std::cout << "COPIA DI fi : " <<std::endl;
-    std::cout << copia << std::endl << std::endl;
-    //std::cout << "COPIA DI fi : "<< copia << std::endl << std::endl;
-    std::cout << fs << std::endl;
+  std::cout << fi << std::endl << std::endl;
+  std::cout << "COPIA DI fi : " <<std::endl;
+  std::cout << copia << std::endl << std::endl;
+  //std::cout << "COPIA DI fi : "<< copia << std::endl << std::endl;
+  std::cout << fs << std::endl;
+  std::cout << a.rimuovi() << std::endl;
+  std::cout << a.rimuovi() << std::endl;
+  std::cout << a.rimuovi() << std::endl;
+  std::cout << a << std::endl;
+
+  std::vector<double> v;
+
+  v.push_back(1.1);
+  v.push_back(2.2);
+  v.push_back(3.3);
+  v.push_back(4.4);
+
+  fifo<double> fd(v.begin(),v.end());
+
+  std::cout << fd ;
+
   return 0;
 }
