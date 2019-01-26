1d3c03040c0379a052e9e49e220d3b91bcb617e6
Stefano Tommasini <groot@MacBookiStefano.lan>
2019-01-26 10:18:30 +0100

    update

--- ProSet/main.cpp
+++ ProSet/main.cpp
@@ -2,8 +2,9 @@
 #include "set.h"
 int main(void) {
   std::cout << "----<Stefano Tommasini 810929>----" << std::endl;
-  Set<int> si;
+  mySet<int> si;
   si.add(1);
   si.add(2);
+  std::cout << si << std::endl;
   return 0;
 }
--- ProSet/set.h
+++ ProSet/set.h
@@ -1,36 +1,77 @@
 #ifndef SET_H
 #define SET_H
 template <typename T>
-class Set {
+class mySet {
 private:
   unsigned int _size;
   unsigned int _occ;
   T * _data;
 
   void init(unsigned int n) {
-    _occ = 0;
-    _size = n;
     _data = new T(n);
+    _size = n;
+    _occ = 0;
   }
 
 public:
-  Set() {
+  mySet() {
     init(5);
   }
-  Set(unsigned int sz) {
+  mySet(unsigned int sz) {
     init(sz);
   }
-  ~Set() {
+
+  mySet(const mySet &other) : _size(0), _data(0), _occ(0) {
+    init(other._size);
+    for (unsigned int i = 0; i < other._occ; i++) {
+      _data[i] = other._data[i];
+    }
+    _occ = other._occ;
+  }
+
+  mySet& operator=(const mySet &other) {
+    if(this != &other) {
+      mySet tmp(other);
+      (*this).swap(tmp);
+    }
+    return *this;
+  }
+
+  ~mySet() {
     delete[] _data;
     init(0);
-  };
+  }
+
+  void swap(mySet &other) {
+    std::swap(_data, other._data);
+    std::swap(_size, other._size);
+    std::swap(_occ, other._occ);
+  }
 
   void add(T val) {
     _data[_occ] = val;
     //std::cout << _data[_occ] << std::endl;
     _occ++;
-
   }
 
+  unsigned int size(void) const {
+    return _size;
+  }
+
+  const T &operator[](unsigned int index) const {
+		assert(index < _size); // pre-condizioni
+		return _data[index];
+	}
+
 };
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const mySet<T> &st) {
+  os << "size: " << st.size() << std::endl;
+	for (unsigned int i = 0; i < st.size(); ++i)
+		os << st[i] << " ";
+
+	return os;
+}
+
 #endif
